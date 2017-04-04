42d73c84f6c9b38d8ddb180f62801ec7e41a7e25
Ines <ines.opcoelho@gmail.com>
2017-04-04 01:46:11 +0100

    Modify navbar

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(function() {
+    $("#navHome").click(function() {
+        // remove classes from all
+        $("li").removeClass("active");
+    });
+
+    $(".navOption").click(function() {
+        // remove classes from all
+        $(".navOption").removeClass("active");
+        // add class to the one we clicked
+        $(this).addClass("active");
+    });
+
+
+    $(".langOption").click(function() {
+        // remove classes from all
+        $(".langOption").removeClass("active");
+        // add class to the one we clicked
+        $(this).addClass("active");
+    });   
+});
+
+
+/**
+ * Listen to scroll to change header opacity class
+ */
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
